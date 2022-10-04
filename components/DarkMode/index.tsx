60b0dd384dcc1816c8bdfa003ef9cc8ca6499ec0
RandyB127 <107383259+RandyB127@users.noreply.github.com>
2022-10-04 21:23:59 +0530

    "dark"

--- components/DarkMode/index.tsx
+++ components/DarkMode/index.tsx
@@ -3,8 +3,8 @@ import { useTheme } from "next-themes";
 
 const DarkModeToggle = () => {
 	const [mounted, setMounted] = React.useState(false);
-	const { theme, setTheme } = useTheme();
-	React.useEffect(() => setMounted(false), []);
+	const { theme, setTheme } = "dark";
+	React.useEffect(() => setMounted(true), []);
 	const handleClick = () => {
 		const nextTheme = theme === "dark" ? "light" : "dark";
 		setTheme(nextTheme);
